8ab3ecdd43d78b7ee135eaa41ad47474fa09914e
Blank <69070442+blankminecraft@users.noreply.github.com>
2023-05-08 17:39:29 -0500

    #1 added printing for maps, added minor color support

---
+++ src/color.py
@@ -0,0 +1,10 @@
+from colorama import Fore, Back
+
+FULL_RESET = f'{Fore.RESET}{Back.RESET}'
+
+F_DEFAULT = Fore.RESET
+F_RED = Fore.RED
+
+B_DEFAULT = Back.RESET
+B_RED = Back.RED
+
--- src/maps.py
+++ src/maps.py
@@ -1,8 +1,25 @@
+from src.color import F_DEFAULT, FULL_RESET, B_DEFAULT
+
+
 class Map:
-    def __init__(self, x, y):
-        self.x_range = range(1, x)
-        self.y_range = range(1, y)
+    def __init__(self, x: int, y: int, base_tile='-'):
+        self.x_range = range(1, x+1)
+        self.y_range = range(1, y+1)
+        self.base_tile = base_tile
+        self.f_base_color = F_DEFAULT
+        self.b_base_color = B_DEFAULT
+        self.cords = {}
+        for yr in self.y_range:
+            for xr in self.x_range:
+                self.cords[xr, yr] = {'tile': self.base_tile, 'f_color': self.f_base_color, 'b_color': self.b_base_color}
+
+    def print(self):
+        for current_key in self.cords.keys():
+            cord_key = self.cords.get(current_key)
+            if current_key[0] == self.x_range[-1]:
+                print(f"{cord_key['f_color']}{cord_key['b_color']}{cord_key['tile']}{FULL_RESET}", end='\n')
+            else:
+                print(f"{cord_key['f_color']}{cord_key['b_color']}{cord_key['tile']}{FULL_RESET}", end='')
 
 
-test = Map(10)
-print(test.x_range)
+
